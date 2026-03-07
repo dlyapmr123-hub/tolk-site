22f75902f2172f1610182fefd02a7e124d5abf2b
dlyapmr123-hub <dlyapmr123@gmail.com>
2026-03-08 00:15:48 +0500

    Исправил критические ошибки

--- public/config.example.js
+++ public/config.example.js
@@ -1,11 +1,8 @@
-// Импортируем Firebase из CDN
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-    
-    const firebaseConfig = {
-        apiKey: "AIzaSyCVl5UQVPWKEh-sP72pUN08uqTF7VcY43Y",
-        authDomain: "tolk-1.firebaseapp.com",
-        projectId: "tolk-1",
-        storageBucket: "tolk-1.firebasestorage.app",
-        messagingSenderId: "79835817899",
-        appId: "1:79835817899:web:28cd02b155f1d7428754ee"
-    };
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSy...",
+    authDomain: "your-project.firebaseapp.com",
+    projectId: "your-project",
+    storageBucket: "your-project.appspot.com",
+    messagingSenderId: "000000000000",
+    appId: "1:000000000000:web:0000000000000000000000"
+};
