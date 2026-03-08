ff49abcaa051e29ce13657f406ed146f2c34c55e
dlyapmr123-hub <dlyapmr123@gmail.com>
2026-03-08 16:58:55 +0500

    Принудительное добавление иконок

---
+++ public/config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCVl5UQVPWKEh-sP72pUN08uqTF7VcY43Y",
+    authDomain: "tolk-1.firebaseapp.com",
+    projectId: "tolk-1",
+    storageBucket: "tolk-1.firebasestorage.app",
+    messagingSenderId: "79835817899",
+    appId: "1:79835817899:web:28cd02b155f1d7428754ee"
+};
